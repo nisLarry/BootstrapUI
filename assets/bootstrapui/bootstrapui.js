300b8d49b4edda81f772f9f6d0600152e88faecb
Larry.Li <nis131914@hotmail.com>
2016-03-05 19:17:16 +0800

    基本ui

---
+++ assets/bootstrapui/bootstrapui.js
@@ -0,0 +1,82 @@
+(function ($) {
+
+    function runFunction(functionToCheck) {
+        var getType = {};
+        if(functionToCheck && getType.toString.call(functionToCheck) === '[object Function]'){
+            functionToCheck();
+        }
+    }
+
+    $.bootstrapui_radio_group = function(radioName,radioSelector,callback){
+        var radioItems = "";
+        for(var i =0; i<radioSelector.length; i++){
+            radioItems += "<div class='radio'><label><input type='radio' name='"+radioName+"radio' id='"+radioName+i+"' value ='"+i+"'>"+radioSelector[i]+"</label></div>";
+        }
+        var radioGroup = "<div class='form-group'>"+radioItems+"</div>";
+        runFunction(callback);
+        return radioGroup;
+    };
+
+    $.bootstrapui_radio_inline_group = function(radioName,radioSelector,callback){
+        var radioItems = "";
+        for(var i =0; i<radioSelector.length; i++){
+            radioItems += "<label class='radio-inline'><input type='radio' name='"+radioName+"radio' id='"+radioName+i+"' value ='"+i+"'>"+radioSelector[i]+"</label>";
+        }
+        var radioGroup = "<div class='form-group'>"+radioItems+"</div>";
+        runFunction(callback);
+        return radioGroup;
+    };
+
+
+    $.bootstrapui_checkbox_group = function(checkboxSelector,callback){
+        var checkboxItems = "";
+        for(var i =0; i<checkboxSelector.length; i++){
+            checkboxItems += "<div class='checkbox'><label><input type='checkbox' value ='"+i+"'>"+checkboxSelector[i]+"</label></div>";
+        }
+        var checkboxGroup = "<div class='form-group'>"+checkboxItems+"</div>";
+        runFunction(callback);
+        return checkboxGroup;
+    };
+
+    $.bootstrapui_checkbox_inline_group = function(checkboxSelector,callback){
+        var checkboxItems = "";
+        for(var i =0; i<checkboxSelector.length; i++){
+            checkboxItems += "<label class='checkbox-inline'><input type='checkbox' value ='"+i+"'>"+checkboxSelector[i]+"</label>";
+        }
+        var checkboxGroup = "<div class='form-group'>"+checkboxItems+"</div>";
+        runFunction(callback);
+        return checkboxGroup;
+    };
+
+    $.bootstrapui_textbox = function(textboxtype,placeholder,callback){
+        var textbox = "<input type='"+textboxtype+"' class='form-control' placeholder='"+placeholder+"'></div>";
+        runFunction(callback);
+        return textbox;
+    };
+
+    $.bootstrapui_button = function(btnlabel,btnclass,callback){
+        var button = "<button class='btn "+btnclass+"' type='submit'>"+btnlabel+"</button>";
+        runFunction(callback);
+        return button;
+    };
+
+    $.bootstrapui_dropbox = function (btnlabel, btnclass, selectfield, callback) {
+        var dropboxselectitem = "";
+        for (var i =0; i < selectfield.length; i++){
+            dropboxselectitem +="<li>"+selectfield[i]+"</li>";
+        }
+        var dropboxselector = "<ul id='bank_deposit_selector' class='dropdown-menu' aria-labelledby='bank_deposit_btn'>" + dropboxselectitem + "</ul>";
+        var dropbutton =
+            "<button class='btn "+btnclass+" dropdown-toggle' type='button' id='bank_deposit_btn' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'><span class='btn-text'>"+btnlabel+"</span><span class='caret'></span></button>" + dropboxselector;
+
+        var dropbox = $("<div class='dropdown'>"+dropbutton+dropboxselector+"</div>");
+
+        dropbox.find("ul>li").on("click",function(e){
+            dropbox.find("span.btn-text").text($(this).text());
+        });
+
+        runFunction(callback);
+        return dropbox;
+
+    };
+})(jQuery);
